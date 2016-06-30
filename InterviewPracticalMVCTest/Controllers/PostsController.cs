2efbec700c1ec0bee301ff76bf844307eff72863
expericolt <simon.coulton@experian.com>
2016-06-30 10:36:29 +0100

    Added project files

---
+++ InterviewPracticalMVCTest/Controllers/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterviewPracticalMVCTest.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: Posts
+        public ActionResult Index()
+        {
+            // Implement requirement 1
+            return View();
+        }
+    }
+}

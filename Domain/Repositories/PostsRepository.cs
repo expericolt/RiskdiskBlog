6280ee08570fd34ec378f83f4061f0d0a2563fef
expericolt <simon.coulton@experian.com>
2016-07-01 11:02:48 +0100

    Added repositories and database files

---
+++ Domain/Repositories/PostsRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Domain.Repositories
+{
+    public class PostsRepository
+    {
+        private readonly DbContext _context;
+
+        public PostsRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Post> GetAllPosts()
+        {
+            return _context.Set<Post>().ToList();
+        }
+    }
+}
--- InterviewPracticalMVCTest/Controllers/PostsController.cs
+++ InterviewPracticalMVCTest/Controllers/PostsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//using Domain.Repositories;
+//using Infrastructure.Data.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -11,10 +13,19 @@ namespace InterviewPracticalMVCTest.Controllers
 {
     public class PostsController : Controller
     {
+        //private readonly PostsRepository _postsRepository;
+            
+        public PostsController()
+        {
+            //_postsRepository = new PostsRepository(new Context());
+        }
+
         // GET: Posts
         public ActionResult Index()
         {
             // Implement requirement 1
+            //var posts = _postsRepository.GetAllPosts();
+
             return View();
         }
     }
